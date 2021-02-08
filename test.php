42610e28a85c25b89e658fa6f394e78a04fc4d2e
interf <test@test.test>
2021-02-08 22:21:39 +0300

    Settings for gitignore

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>test<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
